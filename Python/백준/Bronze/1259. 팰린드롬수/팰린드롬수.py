35af1c16693e9392a9308d5690697021559d2a7e
inyeongjang <135231946+inyeongjang@users.noreply.github.com>
2025-09-03 06:01:49 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ Python/백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,29 +1,13 @@
 n_list = []
 
 while True:
-    n = int(input())
-    if n == 0:
+    n = input().strip()
+    if n == "0":
         break
     n_list.append(n)
 
-for n in n_list: 
-    number = []
-    while (n != 0):
-        number.append(n % 10)
-        n = n // 10
-    
-    a = 0
-    b = len(number) - 1
-    is_same = 1
-
-    while (a < b):
-        if (number[a] != number[b]):
-            is_same = 0
-            break
-        a += 1
-        b -= 1 
-
-    if is_same == 1:
-        print("yes") 
+for n in n_list:
+    if n == n[::-1]:
+        print("yes")
     else:
         print("no")
