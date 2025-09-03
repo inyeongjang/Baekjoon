bf57d4ee5dbb494daf878292b00f39fefe0dee9f
inyeongjang <135231946+inyeongjang@users.noreply.github.com>
2025-09-03 19:10:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 112 ms, Memory: 37104 KB -BaekjoonHub

--- Python/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ Python/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -3,9 +3,4 @@ import string
 S = input()
 
 for ch in string.ascii_lowercase:
-    for i in range(len(S)):
-        index = -1 
-        if S[i] == ch:
-            index = i
-            break 
-    print(index)
+    print(S.find(ch), end=" ")
